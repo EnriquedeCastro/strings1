a6d71554a8105e389cac55eb288554512622cb61
EnriquedeCastro <143217608+EnriquedeCastro@users.noreply.github.com>
2023-09-12 11:38:24 -0700

    Add files via upload

---
+++ RenameThisClass.java
@@ -0,0 +1,25 @@
+
+/**
+ * Write a description of class RenameThisClass here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class RenameThisClass
+{
+    public static void main(String[] args) {
+    String string1 = "I am a string";
+    String string2 = "stringtwo";
+    String string3 = "I am also a string";
+    string1.equals (string2);
+    string1.compareTo(string3);
+    boolean stringsEqual = (string1) == string3;
+        System.out.println(stringsEqual );
+        System.out.println(string1);
+        System.out.println(string2);
+        System.out.println(string3);
+        
+        
+    
+    }
+}
